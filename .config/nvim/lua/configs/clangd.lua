eeeecebdf229ed218b580c7d2ed32725b2cd350f
hosua <hosua.o@gmail.com>
2025-11-22 16:43:44 -0500

    Remove renamed widget

--- .config/nvim/lua/configs/clangd.lua
+++
@@ -1,5 +0,0 @@
-return {
-  cmd = { "clangd", "--compile-commands-dir=." }, -- or path to your JSON
-  filetypes = { "c", "cpp", "objc", "objcpp" },
-  root_dir = require("lspconfig.util").root_pattern("compile_commands.json", ".git"),
-}
