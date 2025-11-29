fb1b579d7e0fe99056fcd5676f4f9aba603a88a1
hosua <hosua.o@gmail.com>
2025-11-29 01:39:00 -0500

    Add show messages button

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -8,6 +8,32 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>sd", vim.diagnostic.open_float, { desc = "Show diagnostics under cursor" })
 map("n", "<leader>sf", vim.lsp.buf.code_action, { desc = "Show/apply code fixes" })
-map("n", "<leader>tt", function()
+
+map({ "n", "i" }, "<leader>sm", function()
+  local MAX_MESSAGES = 50
+
+  local msgs = vim.fn.execute "messages"
+  local lines = vim.split(msgs, "\n")
+
+  local reversed_lines = {}
+  for i = #lines, math.max(#lines - MAX_MESSAGES + 1, 1), -1 do
+    table.insert(reversed_lines, lines[i])
+  end
+
+  vim.cmd "botright new"
+  local buf = vim.api.nvim_get_current_buf()
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, reversed_lines)
+
+  local bo = vim.bo
+  bo.buftype = "nofile"
+  bo.bufhidden = "wipe"
+  bo.swapfile = false
+  bo.modifiable = false
+
+  vim.api.nvim_win_set_height(0, 15)
+  vim.api.nvim_win_set_cursor(0, { 1, 0 })
+end, { desc = "Show messages in bottom pane" })
+
+map({ "n", "i" }, "<leader>tt", function()
   require("base46").toggle_transparency()
 end, { desc = "toggle transparency" })
