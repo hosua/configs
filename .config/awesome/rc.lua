9d4426a569d222792b96d59f64ad9ea5dcafbcc6
hosua <hosua.o@gmail.com>
2025-11-29 18:58:14 -0500

    Disable rmb

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -306,9 +306,9 @@ beautiful.xresources.set_dpi(192)
 -- {{{ Mouse bindings
 
 root.buttons(mytable.join(
-	awful.button({}, 3, function()
-		awful.util.mymainmenu:toggle()
-	end),
+	-- awful.button({}, 3, function()
+	-- 	awful.util.mymainmenu:toggle()
+	-- end),
 	awful.button({}, 4, awful.tag.viewnext),
 	awful.button({}, 5, awful.tag.viewprev)
 ))
