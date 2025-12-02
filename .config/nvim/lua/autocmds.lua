49290cc33626cf153170a0dc09eb9df2ad66d052
Josh Ortiga <jortiga@wellstat.io>
2025-12-02 18:40:51 -0500

    Add signatureHelpProvider check before getting sig

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,1 +1,17 @@
 require "nvchad.autocmds"
+
+vim.api.nvim_clear_autocmds({ event = "TextChangedI", pattern = "*" })
+
+vim.api.nvim_create_autocmd("TextChangedI", {
+  pattern = "*",
+  callback = function()
+    local clients = vim.lsp.get_clients({ bufnr = 0 })
+    for _, client in ipairs(clients) do
+      if client.server_capabilities.signatureHelpProvider then
+        vim.lsp.buf.signature_help()
+        break
+      end
+    end
+  end,
+  desc = "Show signature help if supported",
+})
