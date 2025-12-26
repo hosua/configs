7b1943c0025553bb5e0fe0c48a2a1ab5b75d58f7
hosua <hosua.o@gmail.com>
2025-12-26 01:27:30 -0500

    Add smear plugin

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -103,6 +103,7 @@ awful.spawn.with_shell(
 local modkey = "Mod4"
 local altkey = "Mod1"
 local terminal = "kitty"
+-- local terminal = "st"
 local vi_focus = true -- vi-like client focus https://github.com/lcpz/awesome-copycats/issues/275
 local cycle_prev = true -- cycle with only the previously focused client or all https://github.com/lcpz/awesome-copycats/issues/274
 local editor = os.getenv("EDITOR") or "nvim"
